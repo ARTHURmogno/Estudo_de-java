a5b5f0791417a980e91bb94c861fa0a74ea5dca9
arthur <arthursantosstx2.0@gmail.com>
2025-11-25 02:46:51 -0300

    nada muito grande hoje, não e fácil porem támbem não é difícil.

---
+++ Estruturas_de_Controle_em_Java/Explicação/src/IfElse.java
@@ -0,0 +1,26 @@
+public class IfElse {
+
+
+    /*
+     *   if Else
+     * 
+     * if vocé determina um acontesimento se ele não acontecer então 
+     * então o parametro do Else prosseguirá é executará.
+     * 
+     * if (si), Else (si não)
+     * 
+     * ( Escolha um Número de 1 a 4.
+     * 
+     * Ex: if (opcao < 1 || opcao > 4) {
+     *    System.out.println("opção Valida: "); {
+     * } Else {
+     *    System.out.println("Opção invalida: ");
+     * }
+     * 
+     * )    Se for false o if então o Else Executará. 
+     *
+     * 
+     * 
+     */
+    
+}
---
+++ Estruturas_de_Controle_em_Java/Explicação/src/SwitchCase.java
@@ -0,0 +1,46 @@
+public class SwitchCase {
+
+ /*
+ *  switch case
+ *
+ * Eu penso que é bom para trabalhar com múltiplas escolhas:
+ * 
+ * coda escolha que você faz tem seus parametros é tem um objetivo:
+ * Você pode determinar quantas opções você terá, está por Ex: tem 7 opçôes:
+ * 
+ * 
+ * Ex:
+ * 
+ * switch (opcao) {
+            case 1: 
+            System.out.println("Domimgo ");
+            System.out.println("Fim de Semana uhuull \\o/");
+            break;
+            case 2:
+            System.out.println("Segunda ");
+            break;
+            case 3:
+            System.out.println("Terça ");
+            break;
+            case 4:
+            System.out.println("Quarta ");
+            break;
+            case 5:
+            System.out.println("Quinta ");
+            break;
+            case 6:
+            System.out.println("Sexta ");
+            break;
+            case 7:
+            System.out.println("Sábado ");
+            System.out.println("Fim de Semana uhuull \\o/");
+            break;
+            default:
+            System.out.println("Opção inválida! ");
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+}
