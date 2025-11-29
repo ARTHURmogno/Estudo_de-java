e863202b09600ddb3966b64b1e47251f57a291fd
arthur <arthursantosstx2.0@gmail.com>
2025-11-28 23:03:54 -0300

    Uma Nova Pasta Criada é Um Novo Projeto src

--- Estruturas_de_Controle_em_Java/Projetos-á-Parte/src/SistemaBancárioCadastrosDeClientes.java
+++ Estruturas_de_Controle_em_Java/Projetos-á-Parte/src/SistemaBancárioCadastrosDeClientes.java
@@ -5,16 +5,36 @@ public class SistemaBancárioCadastrosDeClientes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Preencha os Compos Abaixo Com As Devidas informaçãos De Cadastro: ");
+
         System.out.println("Informe Seu Nome: ");
         String nome = sc.nextLine();
+        if (nome == null || nome.trim().isEmpty()) {
+            System.out.println("Erro: Nome é obrigatório. ");
+            return;
+        }
 
         System.out.println("Informe Um CPF Valido: ");
         String cpf = sc.nextLine();
+        if (cpf == null || cpf.trim().isEmpty()) {
+            System.out.println("Erro: cpf é obrigatorio. ");
+            return;
+        }
+        if (!cpf.matches("[0-9]{11}")) {
+            System.out.println("Erro: cpf deve conter exatomente 11 número. ");
+            return;
+        }
 
         System.out.println("Informe Um Email Valido");
         String email = sc.nextLine();
+        if (email == null || email.trim().isEmpty()) {
+            System.out.println("Erro: email é Obrigatorio. ");
+        }
 
-        //TODO: Crie a estrutura condicional para verificar se algum dos campos está vazio:
+        System.out.println("Cadastro validado com sucesso: ");
+
+
+        
 
         sc.close();
     }
