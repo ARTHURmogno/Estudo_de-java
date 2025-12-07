4e5787b7a3f4d359d4e49159853d1ffa2c88d93c
arthur <arthursantosstx2.0@gmail.com>
2025-12-07 15:59:34 -0300

    Java e a Arte da Abstração com Classes e Encapsulamento:
    Criando a primeira Classe

---
+++ Estruturas_de_Controle_em_Java/Abstração-Com-Classes-E-Encapsulamento/src/Pessoa.java
@@ -0,0 +1,31 @@
+public class Pessoa {
+
+    private String nome;
+
+    private int idade;
+
+    /*
+    *   //GETTER
+    *   usado para obter o valor de um atributo privado.
+    *   ele quer o valar da variável. 
+    * */
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+
+    }
+    
+    
+}
---
+++ Estruturas_de_Controle_em_Java/Abstração-Com-Classes-E-Encapsulamento/src/Primeiros-Passos/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        var masculino = new Pessoa();
+        masculino.setNome("léo");
+        masculino.setIdade(22);
+        
+        var feminino = new Pessoa();
+        feminino.setNome("juju");
+        feminino.setIdade(19);
+
+        System.out.println("feminino nome: " + feminino.getNome() + " masculino nome: " + masculino.getNome());
+    }
+    
+}
