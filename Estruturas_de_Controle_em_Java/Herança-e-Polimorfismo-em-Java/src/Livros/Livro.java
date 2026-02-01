d59d126feb40a8369d7bd938d70621414f8a1811
arthur <arthursantosstx2.0@gmail.com>
2026-02-01 19:05:12 -0300

    introdução à POO com classe Livro, atributos, construtor e instância no main.

---
+++ Estruturas_de_Controle_em_Java/Herança-e-Polimorfismo-em-Java/src/Livros/Livro.java
@@ -0,0 +1,13 @@
+package Livros;
+
+public class Livro {
+
+    public String titulo;
+    public int paginas;
+
+    public Livro(String titulo, int paginas) {
+        this.titulo = titulo;
+        this.paginas = paginas;
+    }
+    
+}
---
+++ Estruturas_de_Controle_em_Java/Herança-e-Polimorfismo-em-Java/src/Livros/Main.java
@@ -0,0 +1,12 @@
+package Livros;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Livro l1 = new Livro("Alice", 300);
+
+
+        System.out.printf("Temos um Livro: %s com %d paginas.%n ", l1.titulo, l1.paginas);
+    }
+    
+}
