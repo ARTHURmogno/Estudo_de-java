ca820aed717e322be9c49f4a340fe731b9307df9
arthur <arthursantosstx2.0@gmail.com>
2026-02-01 17:15:05 -0300

    feat: comparar HashSet, LinkedHashSet e TreeSet na prática
    - Demonstrei diferença de ordenação entre os tipos de Set
    - Testei comportamento com valores duplicados
    - Usei conversão entre coleções para análise
    -

---
+++ Estruturas_de_Controle_em_Java/SET/src/ImprimindoComHashSetELinkedHashSetETreeSet.java
@@ -0,0 +1,37 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+public class ImprimindoComHashSetELinkedHashSetETreeSet {
+    public static void main(String[] args) {
+        Set<Integer> numeros01 = new LinkedHashSet<>();
+        //Set<Integer> numeros02 = new HashSet<>(numeros01);
+        //Set<Integer> numeros03 = new TreeSet<>(numeros02);
+
+        numeros01.add(50);
+        numeros01.add(10);
+        numeros01.add(40);
+        numeros01.add(20);
+        numeros01.add(60);
+        numeros01.add(30);
+        numeros01.add(50);
+        numeros01.add(50);
+
+        Set<Integer> numeros02 = new HashSet<>(numeros01);
+        Set<Integer> numeros03 = new TreeSet<>(numeros02);
+
+        System.out.println("Print do HashSet: " + numeros02);
+        System.out.println("Print do LinkedHashSet: " + numeros01);
+        System.out.println("Print do TreeSet: " + numeros03);
+        
+
+        
+
+        
+
+
+        
+    }
+    
+}
