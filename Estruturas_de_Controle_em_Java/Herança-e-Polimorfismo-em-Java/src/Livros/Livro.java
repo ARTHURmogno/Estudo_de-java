927d268562e092bb359e240134565193b2e02279
arthur <arthursantosstx2.0@gmail.com>
2026-02-05 21:49:41 -0300

    "Class Livro: entendendo Regras de negócio em Poo."

--- Estruturas_de_Controle_em_Java/Herança-e-Polimorfismo-em-Java/src/Livros/Livro.java
+++ Estruturas_de_Controle_em_Java/Herança-e-Polimorfismo-em-Java/src/Livros/Livro.java
@@ -6,8 +6,23 @@ public class Livro {
     public int paginas;
 
     public Livro(String titulo, int paginas) {
+        if (paginas < 1) {
+            throw new IllegalArgumentException("Páginas Inválidas.");
+        }
+        if (titulo == null || titulo.isBlank()) {
+            throw new IllegalArgumentException("O nome é obrigatório e não pode estar vazio. ");
+        }
         this.titulo = titulo;
         this.paginas = paginas;
     }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
     
 }
--- Estruturas_de_Controle_em_Java/Herança-e-Polimorfismo-em-Java/src/Livros/Main.java
+++ Estruturas_de_Controle_em_Java/Herança-e-Polimorfismo-em-Java/src/Livros/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         Livro l1 = new Livro("Alice", 300);
+        Livro l2 = new Livro("juju",0);
 
 
-        System.out.printf("Temos um Livro: %s com %d paginas.%n ", l1.titulo, l1.paginas);
+        System.out.printf("Livro: %s com %d paginas.%n ", l1.getTitulo(), l1.getPaginas());
+        System.out.printf("Livro: %s com %d paginas. %n", l2.getTitulo(), l2.getPaginas());
         System.out.println("Poo" );
     }
     
