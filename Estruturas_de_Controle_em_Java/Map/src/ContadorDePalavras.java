d63a970176aea25dea16cf8b9e36208f1e609127
arthur <arthursantosstx2.0@gmail.com>
2026-03-03 21:38:21 -0300

    contador de palavras com Map;

---
+++ Estruturas_de_Controle_em_Java/Map/src/ContadorDePalavras.java
@@ -0,0 +1,37 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ContadorDePalavras {
+    public static void main(String[] args) {
+       /*  Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe uma frase. ");
+        String texto = sc.nextLine();
+
+        texto = texto.toLowerCase();
+
+        String[] palavras = texto.split(" ");
+
+        Map<String, Integer> contador = new HashMap<>();
+
+        for (String p : palavras) {    
+
+            if (contador.containsKey(p)) {
+                contador.put(p, contador.get(p) + 1);
+            } else {
+                contador.put(p, 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entrada : contador.entrySet()) {
+            System.out.println(entrada.getKey() + " apareceu " + entrada.getValue() +  "vezes");
+        }
+
+        sc.close();
+
+             */  
+
+    }
+
+}
