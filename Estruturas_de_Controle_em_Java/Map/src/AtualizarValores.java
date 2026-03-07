7977e2f802d60db97f9d8a8d980acff1eb68f0da
arthur <arthursantosstx2.0@gmail.com>
2026-03-06 21:30:50 -0300

    adicionar iteração em Map usando Map.Entry
    - Criado exemplo utilizando HashMap
    - Inseridos linguagems e quantidade de Votos para tal
    - Utilizado Map.Entry para acessar chave e valor
    - entry.getKey() mostra o nome da linguagem
    - entry.getValue() mostra a quantidade de Votos

--- Estruturas_de_Controle_em_Java/Map/src/AtualizarValores.java
+++ Estruturas_de_Controle_em_Java/Map/src/AtualizarValores.java
@@ -8,31 +8,53 @@ public class AtualizarValores {
         Scanner sc = new Scanner(System.in);
 
         Boolean funcionando = true;
-        Integer java = 0;
-        Integer python = 0;
 
         while(funcionando) {
         System.out.println("Escolha entre: java é python.");
-        System.out.println(" 1 Java: ");
-        System.out.println(" 2 Python: ");
-        Integer opçao = sc.nextInt();
+        System.out.println(" 1 - Java: ");
+        System.out.println(" 2 - Python: ");
+        System.out.println(" 3 - C#: ");
+        System.out.println(" 0 - Sair. ");
+        int opçao = sc.nextInt();
 
         switch(opçao) {
             case 1:
-                if (votos.containsKey(opçao) || opçao.equals(java)) {
-                    votos.put(Value(opçao) 1);
+
+                if (votos.containsKey("java")) {
+                    votos.put("java", votos.get("java") + 1);
+                } else {
+                    votos.put("java", 1);
                 }
+                break;
+            case 2:
+                if (votos.containsKey("pythin")) {
+                    votos.put("python", votos.get("python") + 1);
+                } else {
+                    votos.put("python", 1);
+                }
+                break;
+            case 3:
+                if (votos.containsKey("c#")) {
+                     votos.put("c#", votos.get("c#") + 1);
+                } else {
+                    votos.put("c#", 1);
+                }
+                break;
+                case 0:
+                    funcionando = false;
+                    break;
+                default: 
+                System.out.println("Opção inválida. ");
 
         }
 
-       
     }
 
+    for (Map.Entry<String, Integer> entry : votos.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
 
-
-
-    
-
+    sc.close();
         
     }
     
