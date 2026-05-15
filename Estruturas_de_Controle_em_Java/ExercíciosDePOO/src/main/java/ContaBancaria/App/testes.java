d51aaef70e8efb64547464bd997e2cb8e505364b
arthur <arthursantosstx2.0@gmail.com>
2026-05-14 21:31:13 -0300

    Começo da criação do UsuarioDAO e início da prática de estudos de banco de dados e entendimento do try-catch.

--- Estruturas_de_Controle_em_Java/ExercíciosDePOO/src/main/java/ContaBancaria/App/testes.java
+++ Estruturas_de_Controle_em_Java/ExercíciosDePOO/src/main/java/ContaBancaria/App/testes.java
@@ -1,4 +1,4 @@
-package ContaBancaria.App;
+/*package ContaBancaria.App;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -24,7 +24,7 @@ public class testes {
         }
     
 }
-}
+} */
 
 
 
---
+++ Estruturas_de_Controle_em_Java/ExercíciosDePOO/src/main/java/ContaBancaria/UsuarioDAO/Dao.java
@@ -0,0 +1,10 @@
+package ContaBancaria.UsuarioDAO;
+
+import ContaBancaria.Database.Conexao;
+import java.sql.Connection;
+
+public class Dao {
+
+    Connection conn = Conexao.conectar();
+    
+}
