0d20ce2637f16aaeee4e04290a2b08db181806bb
arthur <arthursantosstx2.0@gmail.com>
2026-05-15 22:18:29 -0300

    feat: implementa método salvar no UsuarioDAO com JDBC

--- Estruturas_de_Controle_em_Java/ExercíciosDePOO/src/main/java/ContaBancaria/UsuarioDAO/Dao.java
+++ Estruturas_de_Controle_em_Java/ExercíciosDePOO/src/main/java/ContaBancaria/UsuarioDAO/Dao.java
@@ -1,10 +1,46 @@
 package ContaBancaria.UsuarioDAO;
 
+import ContaBancaria.Modelo.Usuario;
 import ContaBancaria.Database.Conexao;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class Dao {
 
-    Connection conn = Conexao.conectar();
+
+
+    public void salvar(Usuario usuario) {
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+              conn = Conexao.conectar();
+
+            String sql = "INSERT INTO usuarios (nome, saldo) VALUES (?, ?)";
+              stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, usuario.getNome());
+            stmt.setDouble(2, usuario.getSaldo());
+
+            stmt.executeUpdate();
+
+        } catch (Exception e) {
+            System.out.println("DEU ERRO: AÓ INSERI DADOS.");
+        } finally {
+            try {
+            if (stmt != null) {
+                stmt. close();
+            }
+
+            if (conn != null) {
+                conn.close();
+            }
+            
+            } catch (Exception e) {
+                System.out.println("Erro ao fachar.");
+            }
+        }
+    }
     
 }
