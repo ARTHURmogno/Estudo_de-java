cd58868d66306adba54e3492448a6acf7710bbeb
arthur <arthursantosstx2.0@gmail.com>
2026-05-16 19:09:11 -0300

    "feat: implementa listar() e corrige erro de ordem dos parâmetros identificado em debug"

--- Estruturas_de_Controle_em_Java/ExercíciosDePOO/src/main/java/ContaBancaria/UsuarioDAO/Dao.java
+++ Estruturas_de_Controle_em_Java/ExercíciosDePOO/src/main/java/ContaBancaria/UsuarioDAO/Dao.java
@@ -4,6 +4,9 @@ import ContaBancaria.Modelo.Usuario;
 import ContaBancaria.Database.Conexao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Dao {
 
@@ -26,21 +29,61 @@ public class Dao {
             stmt.executeUpdate();
 
         } catch (Exception e) {
-            System.out.println("DEU ERRO: AÓ INSERI DADOS.");
+            System.out.println("Deu erro.");
         } finally {
             try {
-            if (stmt != null) {
-                stmt. close();
-            }
 
-            if (conn != null) {
-                conn.close();
-            }
-            
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
+
             } catch (Exception e) {
                 System.out.println("Erro ao fachar.");
             }
         }
+    }
+
+    public List<Usuario> listar() {
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try{
+
+            List<Usuario> usuarios = new ArrayList<>();
+
+            conn = Conexao.conectar();
+
+            String sql = "SELECT * FROM usuarios";
+            stmt = conn.prepareStatement(sql);
+
+            ResultSet rs = stmt.executeQuery();
+
+
+            while(rs.next()) {
+
+                int id = rs.getInt("id");
+                String nome = rs.getString("nome");
+                double saldo = rs.getDouble("saldo");
+
+                Usuario u = new Usuario(nome, id, saldo);
+
+                usuarios.add(u);
+
+            }
+            return usuarios;
+
+        } catch (Exception e) {
+            System.out.println("Erro ao lista.");
+            return new ArrayList<>();
+        } finally {
+            try {
+                if (conn != null) conn.close();
+                if (stmt != null) stmt.close();
+            } catch (Exception e) {
+                System.out.println("Erro ao fechar.");
+            }
+        }
+
     }
     
 }
